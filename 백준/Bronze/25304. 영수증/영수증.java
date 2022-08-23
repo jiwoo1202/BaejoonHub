b77071e7a1c3beacd15fdfc205e1ea1e181a28bf
jiwoo1202 <111267434+jiwoo1202@users.noreply.github.com>
2022-08-23 19:05:16 +0900

    [Bronze V] Title: 영수증, Time: 220 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+    class Main {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		int arr[]= new int[N];
+		int sum =0;
+		
+		for(int i=0;i<N;i++) {
+			int a =sc.nextInt();
+			int b =sc.nextInt();
+			arr[i]=a*b;
+			sum +=arr[i];
+		}
+	
+			if(sum==X) {
+			System.out.println("Yes");
+			}
+			else{
+				System.out.println("No");
+			}
+}
+}

faa9bd1d298410621f3646313d1179cb5b2d0047
Jiwoo <111267434+jiwoo1202@users.noreply.github.com>
2022-09-18 00:21:19 +0900

    [Silver V] Title: 소수 찾기, Time: 216 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int cnt = 0;
+		
+		for(int i =0;i<N;i++) {
+			boolean isPrime = true;
+			
+			int num = sc.nextInt();
+			
+			if(num == 1) {
+				continue;
+			}
+		
+			for(int j= 2; j<=Math.sqrt(num);j++) {
+				if(num % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if(isPrime) {
+				cnt++;
+			}
+		}
+			System.out.println(cnt);
+		
+}
+}

ebf6a965bce82314b9cc24206addbbf4372d96b2
Jiwoo <111267434+jiwoo1202@users.noreply.github.com>
2022-09-21 03:00:55 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 608 ms, Memory: 22456 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for(int i=1;i<=num;i++) {
+			for(int k=0;k<num-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<i*2-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=num-1;i>0;i--) {
+			for(int k=0;k<num-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<i*2-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+			}
+			
+		}
+		
+	
+
+

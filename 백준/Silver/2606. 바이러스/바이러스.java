91bfa4b5e55d9fba710bbb2eefb5cf3ee9739276
Jiwoo <111267434+jiwoo1202@users.noreply.github.com>
2023-07-04 23:52:14 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,44 +2,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int n,m;
-    static int[] arr;
-    static int[][] firstsecond;
-    static boolean[] visited;
-    static int result;
-    static int start;
+    static boolean[] visited; // 방문
+    static int[][] arr; //배열
+    static int count;
+    static int next;
+    static Queue<int[]> que = new LinkedList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 7
+        st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken()); // 6
 
-        n= Integer.parseInt(st.nextToken()); // 7
-        st =new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken()); //6
         visited = new boolean[n+1];
-        firstsecond = new int[n+1][n+1];
-        start = 1;
-        //값 넣기
+        arr = new int[n+1][n+1];
+
         for(int i=0;i<m;i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());  //1
-            int b = Integer.parseInt(st.nextToken()); // 2
-            firstsecond[a][b] = firstsecond[b][a] =  1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[a][b]= arr[b][a] =1;
         }
+
         dfs(1);
-        System.out.println(result-1);
+        System.out.println(count);
     }
-    static void dfs(int index){
-        visited[index] = true;
-        result ++;
+    static void dfs(int start){
+        visited[start] = true;
         for(int i=1;i<n+1;i++){
-            if(firstsecond[index][i] == 1 && !visited[i]){
+            if(!visited[i]&&arr[start][i]==1){
+                visited[i] = true;
+                count++;
                 dfs(i);
             }
         }
-
     }
 }

c8bcb52b0a40105964d13a7f2329c4ba04261714
Jiwoo <111267434+jiwoo1202@users.noreply.github.com>
2023-07-10 00:40:27 +0900

    [Silver V] Title: 단어 정렬, Time: 656 ms, Memory: 27204 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,44 +1,37 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Comparator;
- 
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		String[] arr = new String[N];
- 
-		in.nextLine();	// 개행 버림
- 
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextLine();
-		}
-		
-		Arrays.sort(arr, new Comparator<String>() {
-			public int compare(String s1, String s2) {
-				// 단어 길이가 같을 경우 
-				if (s1.length() == s2.length()) {
-					return s1.compareTo(s2);
-				} 
-				// 그 외의 경우 
-				else {
-					return s1.length() - s2.length();
-				}
-			}
-		});
- 
- 
-		System.out.println(arr[0]);
-		
-		for (int i = 1; i < N; i++) {
-			// 중복되지 않는 단어만 출력
-			if (!arr[i].equals(arr[i - 1])) {
-				System.out.println(arr[i]);
-			}
-		}
-	}
- 
+
+    static ArrayList<String> list = new ArrayList<>();
+    static HashSet<String> hashSet = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        Comparator<String> c = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length()==o2.length()){
+                    return o1.compareTo(o2);
+                }
+                else{
+                    return o1.length()-o2.length();
+                }
+            }
+        };
+
+        for(int i=0;i<num;i++){
+            list.add(br.readLine());
+        }
+        Set<String> set= new HashSet<>(list);
+        List<String> newlist = new ArrayList<>(set);
+
+       Collections.sort(newlist,c);
+
+        for(String s : newlist){
+            System.out.println(s);
+        }
+    }
 }

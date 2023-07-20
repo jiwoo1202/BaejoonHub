9c786658e53bc63f87074e897352dcc71773041f
Jiwoo <111267434+jiwoo1202@users.noreply.github.com>
2023-07-21 00:32:34 +0900

    [Silver IV] Title: 로프, Time: 340 ms, Memory: 26616 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,36 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
+    static int n;
+    static int[] A;
+    static int max;
+    static int leng;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-		int[] rope = new int[N];
-
-		for (int i = 0; i < N; i++) {
-			rope[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(rope); 
-		
-		int max = Integer.MIN_VALUE;
-		
-		
-	
-		for (int i = 0; i < N; i++) {
-			max = Math.max(max, rope[i] * (N - i));
-		}
-
-		bw.write(max + "\n");
-		bw.flush();
-		br.close();
-		bw.close();
-	}
-
+        A = new int[n];
+        for(int i=0;i<n;i++){
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(A);
+        leng = A.length; // 4
+        max = A[A.length-1]; // 최대값
+       for(int i=0;i<n;i++){
+           if(A[i]*(leng-i)>max){
+               max = A[i]*(leng-i);
+           }
+       }
+        System.out.println(max);
+    }
 }

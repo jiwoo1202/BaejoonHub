3fe9521bc5105c50fac50dd67a9a4560bb31745f
Jiwoo <111267434+jiwoo1202@users.noreply.github.com>
2023-08-08 01:13:01 +0900

    [Silver II] Title: 촌수계산, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,55 +1,52 @@
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
-	
-	static List<Integer>[] relation;
-	static boolean[] checked;
-	static int res = -1;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		relation = new ArrayList[n+1];
-		checked = new boolean[n+1];
-		for(int i=1; i<n+1; i++) {
-			relation[i] = new ArrayList<>();
-		}
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		
-		int l = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<l; i++) {
-			st = new StringTokenizer(br.readLine());
-			int p = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			relation[p].add(c);
-			relation[c].add(p);
-		}
-		
-		dfs(x,y, 0);
-		System.out.println(res);
-	}
-	
-	static void dfs(int start, int end, int cnt) {
-		if(start == end) {
-			res = cnt;
-			return; 
-		}
-		
-		checked[start] = true;
-		for(int i=0; i<relation[start].size(); i++) { 
-			int next = relation[start].get(i);
-			if(!checked[next]) {
-				dfs(next, end, cnt+1);
-			}
-		}
-	}
+    static int n,x,y,m;
+    static int[][] arr;
+    static boolean[] visited;
+    static int result;
+    static int find = 0;
+    static int count = -1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken()); // 9
+        st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken()); // 7
+        y = Integer.parseInt(st.nextToken()); //3
+
+        m = Integer.parseInt(br.readLine()); // 7
+
+        arr = new int[n+1][n+1];
+        visited = new boolean[n+1];
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = arr[b][a] = 1;
+        }
+
+        dfs(x,y,0);
+        System.out.println(count);
+    }
+  static void dfs(int start, int end,int counts){
+        visited[start] = true;
+      if(start==end){
+          find = 1;
+          count = counts;
+          return;
+      }
+          for(int i=1;i<arr.length;i++){
+              if(!visited[i]&&arr[start][i]==1){
+                  dfs(i,end,counts+1);
+
+              }
+      }
+
+  }
 }

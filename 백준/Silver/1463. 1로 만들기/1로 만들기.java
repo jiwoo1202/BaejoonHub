3a4e62c709216622d0ff526a1fb2df15f3290aa7
Jiwoo <111267434+jiwoo1202@users.noreply.github.com>
2023-08-14 00:48:54 +0900

    [Silver III] Title: 1로 만들기, Time: 200 ms, Memory: 18180 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,42 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
- 
-	static Integer[] dp;
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
- 
-		dp = new Integer[N + 1];
-		dp[0] = dp[1] = 0;
- 
-		System.out.print(recur(N));
- 
-	}
- 
-	static int recur(int N) {
- 
-		if (dp[N] == null) {
-			// 6으로 나눠지는 경우 
-			if (N % 6 == 0) {
-				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-			}
-			// 3으로만 나눠지는 경우 
-			else if (N % 3 == 0) {
-				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
-			}
-			// 2로만 나눠지는 경우 
-			else if (N % 2 == 0) {
-				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
-			}
-			// 2와 3으로 나누어지지 않는 경우
-			else {
-				dp[N] = recur(N - 1) + 1;
-			}
-		}
-		return dp[N];
-	}
- 
+    static int N;
+    static int[] dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N+1];
+        dp[0]=dp[1] = 0;
+        if(N==2){
+            dp[2]=1;
+        }
+        else if(N >=3){
+            dp[3]=dp[2]=1;
+        }
+        for(int i=4;i<=N;i++){
+           dp[i] = dp[i-1]+1; // 2
+            if(i%2==0){
+              dp[i] = Math.min(dp[i],dp[i/2]+1);
+            }
+            if(i%3==0){
+                dp[i] = Math.min(dp[i],dp[i/3]+1);
+            }
+        }
+        System.out.println(dp[N]);
+    }
 }

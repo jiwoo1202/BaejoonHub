a2bad17a88f5aaa8c96676ad8fb91e36b8d189da
Jiwoo <jiwoo12021@naver.com>
2023-09-14 00:51:55 +0900

    [Silver IV] Title: 상근이의 여행, Time: 252 ms, Memory: 31588 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,29 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main
-{
 
-	public static void main(String[] args) throws IOException 
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine().trim());
-		
-		int T = Integer.parseInt(st.nextToken().trim());
-		for(int t=0;t<T;t++)
-		{
-			int N, M;
-			st = new StringTokenizer(br.readLine().trim());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			
-			for(int m=0;m<M;m++)
-			{
-				st = new StringTokenizer(br.readLine());
-			}
-			System.out.println(N-1);
-		}
-	}
+public class Main {
+
+    static int t;
+    static int n,m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        t = Integer.parseInt(st.nextToken());
+
+        for(int i=0;i<t;i++){
+            st= new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+
+            for(int j=0;j<m;j++){
+                st = new StringTokenizer(br.readLine());
+            }
+            System.out.println(n-1);
+        }
+    }
 }

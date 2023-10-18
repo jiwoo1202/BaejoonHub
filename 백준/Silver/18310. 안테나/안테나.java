fd8e7923b86a8a38ecfa766f41aba568a816766a
Jiwoo <jiwoo12021@naver.com>
2023-10-19 00:34:06 +0900

    [Silver III] Title: 안테나, Time: 588 ms, Memory: 41412 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.java
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -1,29 +1,37 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
 
+public class Main {
+    static int N;
+    static int[] house;
+    static int total;
+    static int[] sumList;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] arr = new int[num];
-
-        for (int i = 0; i < num; i++) {
-            arr[i] = Integer.parseInt(st.nextToken(" "));
+        N = Integer.parseInt(st.nextToken());
+        house = new int[N];
+        sumList = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            house[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        Arrays.sort(house);
 
-        if(num % 2 ==0){
-            System.out.println(arr[num/2-1]);
-        }else{
-            System.out.println(arr[num/2]);
+        if(N % 2 ==0){
+            total = N / 2;
+            System.out.println(house[total-1]);
         }
+        else{
+            total = N / 2;
+            System.out.println(house[total]);
+        }
+
 
     }
 }

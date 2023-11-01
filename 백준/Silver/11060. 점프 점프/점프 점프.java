006f9f6425fb07342518d853b4e61aa22d2fc4e6
Jiwoo <jiwoo12021@naver.com>
2023-11-01 23:02:17 +0900

    [Silver II] Title: 점프 점프, Time: 132 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -1,27 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
+
+
 public class Main {
+
+    static int n;
+    static int[] arr;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
-        long[] dp = new long[1101];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+
+        n = Integer.parseInt(st.nextToken()); // 10
+        arr = new int[n+1];
+        dp = new int[n+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = Integer.MAX_VALUE;
         }
+
+        Arrays.fill(dp,Integer.MAX_VALUE-1); // dp에 최대값 채우기
         dp[1] = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dp[i] >= Integer.MAX_VALUE) continue;
-            for (int j = 1; j <= arr[i]; j++) {
-                dp[i+j] = Math.min(dp[i+j], dp[i]+1);
+       // n = 10
+        for(int i=1;i<=n;i++){
+            for(int j=i+1;j<arr[i]+1+i;j++){
+                if(j>n) break;
+                dp[j] = Math.min(dp[j],dp[i]+1);
             }
         }
-        if (dp[n] >= Integer.MAX_VALUE) {
-            System.out.print(-1);
-        } else {
-            System.out.print(dp[n]);
+        if(dp[n]==Integer.MAX_VALUE-1){
+            System.out.println(-1);
+        }
+        else{
+            System.out.println(dp[n]);
         }
     }
 }

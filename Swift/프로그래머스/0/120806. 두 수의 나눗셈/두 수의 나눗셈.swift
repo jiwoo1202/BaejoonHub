bbdc69e833688e851b7c11ec8e679f0a00f670fb
Jiwoo <jiwoo12021@naver.com>
2025-11-20 13:09:36 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- Swift/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
+++ Swift/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func solution(_ num1:Int, _ num2:Int) -> Int {
-    let floatNum1: Float = Float(num1)
-    let floatNum2: Float = Float(num2)
-    return Int((floatNum1 / floatNum2) * 1000)
+   
+    return Int((Double(num1) / Double(num2)) * 1000)
 }
